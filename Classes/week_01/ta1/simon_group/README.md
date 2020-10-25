2bc3cb9d12fb3be92fff3b5cc40e9fc6837b0837
семен семен <simon96pikalov@gmail.com>
2020-10-25 07:53:07 +0200

    added readme for ta

---
+++ Classes/week_01/ta1/simon_group/README.md
@@ -0,0 +1,11 @@
+# Welcome to Ariel OOP Simon group
+
+# Youtube chanel 
+https://www.youtube.com/playlist?list=PLI1YQ3QGdy-Y5KxXuxGUXuh6idMfJIIbB
+
+# Themes 
+
+![](\resources/themes.JPG)
+
+![](\resources/interface.JPG)
+
---
+++ Classes/week_01/ta1/simon_group/src/FastCar.java
@@ -0,0 +1,15 @@
+public class FastCar implements PublicTransfer {
+    @Override
+    public int getCurrPassengerCount() {
+        return 0;
+    }
+
+    @Override
+    public void addPassenger() {
+    }
+
+    @Override
+    public void getOffPassenger() {
+
+    }
+}
