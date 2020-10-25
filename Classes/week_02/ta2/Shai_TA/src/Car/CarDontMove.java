5d588f820514b5dfde78da0bac5411cb52028c9a
Shai <ifryed@gmail.com>
2020-10-26 01:57:48 +0200

    typo

--- Classes/week_02/ta2/Shai_TA/src/Car/CarDontMove.java
+++ Classes/week_02/ta2/Shai_TA/src/Car/CarDontMove.java
@@ -2,6 +2,6 @@ package Car;
 
 public class CarDontMove extends Exception {
     public CarDontMove() {
-        super("Car Cannot Move! Find other car or walk!!");
+        super("Car Cannot Move! Find another car or walk!!");
     }
 }
