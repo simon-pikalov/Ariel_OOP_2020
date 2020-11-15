392a4a1e89e92224f530bf8507e3d1a09fbbe5dd
boazbenmoshe <benmo@g.ariel.ac.il>
2020-11-15 08:03:53 +0200

    Class5 Thread_101 example

--- Classes/week_05/JSonFileWrite1.java
+++ Classes/week_05/JSonFileWrite1.java
@@ -18,104 +18,91 @@ jsonFileRead(fileName);
 }
  
 //writing JSON
-public static void jsonFileWrite(String fileName) throws JSONException
-{
+public static void jsonFileWrite(String fileName) throws JSONException {
  
-JSONObject allEmps=new JSONObject();
+    JSONObject allEmps=new JSONObject();
+    JSONArray empArray=new JSONArray();
+    JSONObject empObj;
+    JSONArray phoneNumbers =null;
+    JSONObject phoneObj;
  
-JSONArray empArray=new JSONArray();
-JSONObject empObj;
-JSONArray phoneNumbers =null;
-JSONObject phoneObj;
+    empObj = new JSONObject();
+    empObj.put("Name", "ABC");
+    empObj.put("Designation", "Manager");
+    empObj.put("Pay", "Rs. 60000/-");
  
-empObj = new JSONObject();
-empObj.put("Name", "ABC");
-empObj.put("Designation", "Manager");
-empObj.put("Pay", "Rs. 60000/-");
+    phoneNumbers= new JSONArray();
+    phoneObj=new JSONObject();
+    phoneObj.put("LandLine", "11-2xxxx99");
+    phoneObj.put("Mobile", "99xxxxxx11");
+    phoneNumbers.put(phoneObj);
  
-phoneNumbers= new JSONArray();
-phoneObj=new JSONObject();
-phoneObj.put("LandLine", "11-2xxxx99");
-phoneObj.put("Mobile", "99xxxxxx11");
-phoneNumbers.put(phoneObj);
+    empObj.put("PhoneNumbers", phoneNumbers);
+    empArray.put(empObj);
  
-empObj.put("PhoneNumbers", phoneNumbers);
+    empObj = new JSONObject();
+    empObj.put("Name", "ABC");
+    empObj.put("Designation", "Sr.Manager");
+    empObj.put("Pay", "Rs. 60000/-");
  
-empArray.put(empObj);
+    phoneNumbers= new JSONArray();
+    phoneObj=new JSONObject();
+    phoneObj.put("LandLine", "12-2xxxx129");
+    phoneObj.put("Mobile", "45xxxxxx11");
+    phoneNumbers.put(phoneObj);
  
-empObj = new JSONObject();
-empObj.put("Name", "ABC");
-empObj.put("Designation", "Sr.Manager");
-empObj.put("Pay", "Rs. 60000/-");
- 
-phoneNumbers= new JSONArray();
-phoneObj=new JSONObject();
-phoneObj.put("LandLine", "12-2xxxx129");
-phoneObj.put("Mobile", "45xxxxxx11");
-phoneNumbers.put(phoneObj);
- 
-empObj.put("PhoneNumbers", phoneNumbers);
- 
-empArray.put(empObj);
- 
-allEmps.put("Employees", empArray);
+    empObj.put("PhoneNumbers", phoneNumbers);
+    empArray.put(empObj);
+    allEmps.put("Employees", empArray);
 
 try {
-
 // Writing to a file
-FileWriter file = new FileWriter(fileName);
-file.write(allEmps.toString());
-file.flush();
-file.close();
+    FileWriter file = new FileWriter(fileName);
+    file.write(allEmps.toString());
+    file.flush();
+    file.close();
  
-System.out.println("JSon file created");
-} catch (IOException e) {
-e.printStackTrace();
+    System.out.println("JSon file created"); }
+    catch (IOException e) {
+        e.printStackTrace();
+    }
 }
- 
-}
-
 //reading JSON
+public static void jsonFileRead(String fileName) throws JSONException {
+    try {
  
-public static void jsonFileRead(String fileName) throws JSONException
-{
+        Scanner scanner = new Scanner( new File(fileName) );
+        String jsonString = scanner.useDelimiter("\\A").next();
+        scanner.close();
+        System.out.println("jsonString =" + jsonString);
+        JSONObject empObj = new JSONObject();
+        JSONObject phoneObj = new JSONObject();
+        JSONObject jsonObject = new JSONObject(jsonString);
  
-try {
+        JSONArray jsonArray= jsonObject.getJSONArray("Employees");
  
-Scanner scanner = new Scanner( new File(fileName) );
-String jsonString = scanner.useDelimiter("\\A").next();
-scanner.close();
-System.out.println("jsonString =" + jsonString);
-JSONObject empObj = new JSONObject();
-JSONObject phoneObj = new JSONObject();
+        for (int j=0; j<jsonArray.length();j++) {
+            empObj =jsonArray.getJSONObject(j);
+            String nameOfCountry = (String) empObj.get("Name");
+            System.out.println("Name : "+nameOfCountry);
  
-JSONObject jsonObject = new JSONObject(jsonString);
+            String population = (String) empObj.get("Designation");
+            System.out.println("Designation: "+population);
  
-JSONArray jsonArray= jsonObject.getJSONArray("Employees");
+            System.out.println("Phone Numbers are :");
+            JSONArray phoneNumbers = (JSONArray) empObj.get("PhoneNumbers");
+            for (int i=0; i<phoneNumbers.length();i++) {
+                phoneObj=phoneNumbers.getJSONObject(i);
+                System.out.println(" Land Line " + phoneObj.get("LandLine"));
+                System.out.println(" Mobile " + phoneObj.get("Mobile"));
+            }
  
-for (int j=0; j<jsonArray.length();j++)
-{
-empObj =jsonArray.getJSONObject(j);
-String nameOfCountry = (String) empObj.get("Name");
-System.out.println("Name : "+nameOfCountry);
+        }
  
-String population = (String) empObj.get("Designation");
-System.out.println("Designation: "+population);
- 
-System.out.println("Phone Numbers are :");
-JSONArray phoneNumbers = (JSONArray) empObj.get("PhoneNumbers");
-for (int i=0; i<phoneNumbers.length();i++)
-{
-phoneObj=phoneNumbers.getJSONObject(i);
-System.out.println(" Land Line " + phoneObj.get("LandLine"));
-System.out.println(" Mobile " + phoneObj.get("Mobile"));
-}
- 
-}
- 
-} catch (FileNotFoundException e) {
-e.printStackTrace();
-}
+    }
+    catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
  
 }
  
---
+++ Classes/week_05/Thread_101.java
@@ -0,0 +1,14 @@
+public class Thread_101 extends Thread {
+    public Thread_101(String name) {
+        super(name);
+    }
+    public void run() {
+        for(int i=0;i<20;i++) {
+            System.out.print(""+i+") ");
+            for(int j=0;j<10;j++) {
+                System.out.print(super.getName());
+            }
+            System.out.println();
+        }
+    }
+}
