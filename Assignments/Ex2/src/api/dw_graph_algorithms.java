c036f4dcff5ddf309daf46e2ca772fdc151c3035
boazbenmoshe <benmo@g.ariel.ac.il>
2020-11-22 08:11:49 +0200

    Ex2 - an interface for the required algorithms on directed weighted graphs was added (dw_graph_algorithms)

---
+++ Assignments/Ex2/src/api/dw_graph_algorithms.java
@@ -0,0 +1,75 @@
+package api;
+import java.util.List;
+/**
+ * This interface represents a Directed (positive) Weighted Graph Theory Algorithms including:
+ * 0. clone(); (copy)
+ * 1. init(graph);
+ * 2. isConnected(); // strongly (all ordered pais connected)
+ * 3. double shortestPathDist(int src, int dest);
+ * 4. List<node_data> shortestPath(int src, int dest);
+ * 5. Save(file); // JSON file
+ * 6. Load(file); // JSON file
+ *
+ * @author boaz.benmoshe
+ *
+ */
+public interface dw_graph_algorithms {
+    /**
+     * Init the graph on which this set of algorithms operates on.
+     * @param g
+     */
+    public void init(directed_weighted_graph g);
+
+    /**
+     * Return the underlying graph of which this class works.
+     * @return
+     */
+    public directed_weighted_graph getGraph();
+    /**
+     * Compute a deep copy of this weighted graph.
+     * @return
+     */
+    public directed_weighted_graph copy();
+    /**
+     * Returns true if and only if (iff) there is a valid path from each node to each
+     * other node. NOTE: assume directional graph (all n*(n-1) ordered pairs).
+     * @return
+     */
+    public boolean isConnected();
+    /**
+     * returns the length of the shortest path between src to dest
+     * Note: if no such path --> returns -1
+     * @param src - start node
+     * @param dest - end (target) node
+     * @return
+     */
+    public double shortestPathDist(int src, int dest);
+    /**
+     * returns the the shortest path between src to dest - as an ordered List of nodes:
+     * src--> n1-->n2-->...dest
+     * see: https://en.wikipedia.org/wiki/Shortest_path_problem
+     * Note if no such path --> returns null;
+     * @param src - start node
+     * @param dest - end (target) node
+     * @return
+     */
+    public List<node_data> shortestPath(int src, int dest);
+
+    /**
+     * Saves this weighted (directed) graph to the given
+     * file name - in JSON format
+     * @param file - the file name (may include a relative path).
+     * @return true - iff the file was successfully saved
+     */
+    public boolean save(String file);
+
+    /**
+     * This method load a graph to this graph algorithm.
+     * if the file was successfully loaded - the underlying graph
+     * of this class will be changed (to the loaded one), in case the
+     * graph was not loaded the original graph should remain "as is".
+     * @param file - file name of JSON file
+     * @return true - iff the graph was successfully loaded.
+     */
+    public boolean load(String file);
+}
